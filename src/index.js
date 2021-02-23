2b741c89fa6ab032d04bb13569bc91d7e495b79f
Shahar T <shahart@codevalue.com>
2021-02-23 12:50:31 +0200

    set up baseurl - setup github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/stocks-analyzer'>
     <App />
   </Router>,
   document.getElementById('root')
